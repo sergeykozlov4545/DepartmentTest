ea48dd4f44d7342bc0564b10ed2fec7a6f7827a5
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-12-12 11:57:57 +0500

    Базовый фрагмент

--- app/src/main/java/com/example/sergey/departmenttest/extansion/Context.kt
+++ app/src/main/java/com/example/sergey/departmenttest/extansion/Context.kt
@@ -1,7 +1,17 @@
 package com.example.sergey.departmenttest.extansion
 
 import android.content.Context
+import android.support.v4.app.Fragment
 import android.widget.Toast
 
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 fun Context.longToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+fun Fragment.toast(message: String) {
+    activity.takeIf { it != null }
+            ?.run { Toast.makeText(this, message, Toast.LENGTH_SHORT).show() }
+}
+fun Fragment.longToast(message: String) {
+    activity.takeIf { it != null }
+            ?.run { Toast.makeText(this, message, Toast.LENGTH_LONG).show() }
+}
---
+++ app/src/main/java/com/example/sergey/departmenttest/feature/core/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.example.sergey.departmenttest.feature.core
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.View
+import com.example.sergey.departmenttest.R
+import com.example.sergey.departmenttest.extansion.toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import java.io.IOException
+import kotlin.coroutines.CoroutineContext
+
+class BaseFragment : Fragment(), CoroutineScope, BaseView {
+
+    private lateinit var job: Job
+    private var isCreatedJob = false
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        job = Job()
+        isCreatedJob = true
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if (isCreatedJob) job.cancel()
+    }
+
+    override fun onError(exception: Exception) {
+        val message = when {
+            exception is IOException -> getString(R.string.connectionError)
+            exception.message.isNullOrEmpty() -> getString(R.string.retryOperation)
+            else -> exception.message
+        }
+        toast(message!!)
+    }
+}
