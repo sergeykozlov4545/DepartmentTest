c2460f753ae59c3f6fbc9793c2ee298965049a2b
Kozlov Sergey <sergeykozlov4545@gmail.com>
2018-12-14 10:50:07 +0500

    Переделка анимации в DotsProgressBar

--- app/src/main/java/com/example/sergey/departmenttest/feature/dotsProgressBar/DotsProgressBar.kt
+++ app/src/main/java/com/example/sergey/departmenttest/feature/dotsProgressBar/DotsProgressBar.kt
@@ -1,21 +1,23 @@
 package com.example.sergey.departmenttest.feature.dotsProgressBar
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
+import android.content.res.TypedArray
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.support.annotation.ColorRes
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import com.example.sergey.departmenttest.R
-import com.example.sergey.departmenttest.feature.animation.AnimationCallback
-import com.example.sergey.departmenttest.feature.animation.RepeatableAnimation
 
 class DotsProgressBar @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleRes: Int = 0
-) : View(context, attrs, defStyleRes), AnimationCallback {
+) : View(context, attrs, defStyleRes) {
 
     companion object {
         private const val DEFAULT_DOT_RADIUS = 15f
@@ -23,47 +25,39 @@ class DotsProgressBar @JvmOverloads constructor(
         private const val DEFAULT_PADDING_DOTS = 10f
         private const val DEFAULT_DOT_COLOR = android.R.color.black
         private const val DEFAULT_COUNT_DOTS = 4
-        private const val DEFAULT_ANIMATION_DURATION = 100
-        private const val ONLY_RIGHT_ANIMATION = 0
-        private const val LEFT_RIGHT_ANIMATION = 1
+        private const val DEFAULT_ANIMATION_DURATION = 250
+        private val DEFAULT_ANIMATION_MODE = AnimationMode.ONLY_RIGHT
     }
 
-    private val dotRadius: Float
-    private val activeDotRadius: Float
-    private val paddingDots: Float
+    private enum class AnimationMode(private val id: Int) {
+        ONLY_RIGHT(0),
+        LEFT_RIGHT(1);
+
+        companion object {
+            fun getModById(id: Int) =
+                    AnimationMode.values().find { it.id == id } ?: ONLY_RIGHT
+        }
+    }
+
+    private var dotRadius: Float = DEFAULT_DOT_RADIUS
+    private var activeDotRadius: Float = DEFAULT_ACTIVE_DOT_RADIUS
+    private var paddingDots: Float = DEFAULT_PADDING_DOTS
     @ColorRes
-    private val colorDot: Int
-    private val countDots: Int
-    private var activeDotsPosition = 0
+    private var colorDot: Int = DEFAULT_DOT_COLOR
+    private var countDots: Int = DEFAULT_COUNT_DOTS
+    private var durationAnimation: Int = DEFAULT_ANIMATION_DURATION
+    private var animationMode: AnimationMode = DEFAULT_ANIMATION_MODE
 
-    private var animation: RepeatableAnimation? = null
-    private val durationAnimation: Int
-
-    private var positionDelta = 1
-    private val animationMode: Int
+    private val dots: MutableList<Dot> = ArrayList()
+    private var dotsAnimator: ValueAnimator? = null
 
     init {
-        context.theme.obtainStyledAttributes(attrs, R.styleable.DotsProgressBar, 0, 0)
-                .apply {
-                    try {
-                        dotRadius = getDimension(R.styleable.DotsProgressBar_dotRadius, DEFAULT_DOT_RADIUS)
-                        activeDotRadius = getDimension(R.styleable.DotsProgressBar_activeDotRadius, DEFAULT_ACTIVE_DOT_RADIUS)
-                        paddingDots = getDimension(R.styleable.DotsProgressBar_paddingDots, DEFAULT_PADDING_DOTS)
-                        @SuppressLint("ResourceAsColor")
-                        colorDot = getColor(R.styleable.DotsProgressBar_colorDot, DEFAULT_DOT_COLOR)
-                        countDots = getInteger(R.styleable.DotsProgressBar_countDots, DEFAULT_COUNT_DOTS)
-                        durationAnimation = getInteger(R.styleable.DotsProgressBar_durationAnimation, DEFAULT_ANIMATION_DURATION)
-                        animationMode = getInt(R.styleable.DotsProgressBar_animationMode, ONLY_RIGHT_ANIMATION)
-                    } finally {
-                        recycle()
-                    }
-                }
+        obtainStyledAttributes(context, attrs)
+        initDots()
     }
 
     @SuppressLint("ResourceAsColor")
-    private val paint = Paint().apply {
-        color = colorDot
-    }
+    private val paint = Paint().apply { color = colorDot }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -74,7 +68,7 @@ class DotsProgressBar @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (visibility == VISIBLE) {
-            drawCircles(canvas)
+            dots.forEach { canvas?.drawCircle(it.center.x, it.center.y, it.radius, paint) }
         }
     }
 
@@ -99,49 +93,79 @@ class DotsProgressBar @JvmOverloads constructor(
         }
     }
 
-    override fun onAnimationRepeat() {
-        when (animationMode) {
-            ONLY_RIGHT_ANIMATION -> {
-                activeDotsPosition += positionDelta
-                if (activeDotsPosition == countDots) {
-                    activeDotsPosition = 0
-                }
-            }
-            LEFT_RIGHT_ANIMATION -> {
-                activeDotsPosition += positionDelta
-                if (activeDotsPosition !in 1 until (countDots - 1)) {
-                    positionDelta *= -1
-                }
-            }
-            else -> {
-            }
+    private fun obtainStyledAttributes(context: Context, attrs: AttributeSet? = null) {
+        attrs.takeIf { it != null }?.let {
+            context.theme.obtainAttributes(it).run { restoreTypedArray() }
         }
     }
 
-    override fun applyTransformation() {
-        invalidate()
+    private fun Resources.Theme.obtainAttributes(attrs: AttributeSet): TypedArray {
+        return obtainStyledAttributes(attrs, R.styleable.DotsProgressBar, 0, 0)
     }
 
-    private fun drawCircles(canvas: Canvas?) {
+    private fun TypedArray.restoreTypedArray() {
+        try {
+            dotRadius = getDimension(R.styleable.DotsProgressBar_dotRadius, DEFAULT_DOT_RADIUS)
+            activeDotRadius = getDimension(R.styleable.DotsProgressBar_activeDotRadius, DEFAULT_ACTIVE_DOT_RADIUS)
+            paddingDots = getDimension(R.styleable.DotsProgressBar_paddingDots, DEFAULT_PADDING_DOTS)
+            @SuppressLint("ResourceAsColor")
+            colorDot = getColor(R.styleable.DotsProgressBar_colorDot, DEFAULT_DOT_COLOR)
+            countDots = getInteger(R.styleable.DotsProgressBar_countDots, DEFAULT_COUNT_DOTS)
+            durationAnimation = getInteger(R.styleable.DotsProgressBar_durationAnimation, DEFAULT_ANIMATION_DURATION)
+            animationMode = AnimationMode.getModById(
+                    getInt(R.styleable.DotsProgressBar_animationMode, -1)
+            )
+        } finally {
+            recycle()
+        }
+    }
+
+    private fun initDots() {
         var marginLeft = 0f
         for (i in 0 until countDots) {
-            val dotRadius = if (i == activeDotsPosition) activeDotRadius else dotRadius
-            canvas?.drawCircle(marginLeft + dotRadius, height.toFloat() / 2, dotRadius, paint)
+            val center = Point(marginLeft + dotRadius, activeDotRadius)
+            dots.add(Dot(center, dotRadius))
             marginLeft += 2 * dotRadius + paddingDots
         }
     }
 
     private fun startAnimation() {
-        if (animation != null) return
-        animation = RepeatableAnimation(durationAnimation, this)
-        startAnimation(animation)
+        dotsAnimator = createDotsAnimator()
+        dotsAnimator?.start()
     }
 
     private fun stopAnimation() {
-        animation.takeIf { it != null }?.run {
-            cancel()
-            detach()
-            animation = null
+        dotsAnimator?.cancel()
+        dotsAnimator = null
+    }
+
+    private data class Point(var x: Float, val y: Float)
+
+    private data class Dot(val center: Point, var radius: Float)
+
+    private fun createDotsAnimator(): ValueAnimator {
+        return ValueAnimator.ofInt(0, dots.size).apply {
+            interpolator = LinearInterpolator()
+            duration = durationAnimation.toLong()
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = if (animationMode == AnimationMode.ONLY_RIGHT) {
+                ValueAnimator.RESTART
+            } else {
+                ValueAnimator.REVERSE
+            }
+            addUpdateListener {
+                setActiveDot(animatedValue as Int)
+                invalidate()
+            }
+        }
+    }
+
+    private fun setActiveDot(position: Int) {
+        var marginLeft = 0f
+        dots.forEachIndexed { index, dot ->
+            dot.radius = if (index == position) activeDotRadius else dotRadius
+            dot.center.x = marginLeft + dot.radius
+            marginLeft += 2 * dot.radius + paddingDots
         }
     }
 }
